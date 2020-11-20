f7d9c62a01213b4c3e66b034fd9c3c546fe0115e
eichelberger <eichelberger@sse.uni-hildesheim.de>
2020-11-20 09:20:56 +0100

    type translation missing

---
+++ platform/transport/transport/src/main/java/de/iip_ecosphere/platform/transport/serialization/TypeTranslator.java
@@ -0,0 +1,23 @@
+package de.iip_ecosphere.platform.transport.serialization;
+
+import java.io.IOException;
+
+/**
+ * Defines a type translator between two given types. Translation may imply a loss or augmentation of information.
+ *
+ * @param <S> the source type
+ * @param <T> the target type
+ * @author Holger Eichelberger, SSE
+ */
+public interface TypeTranslator<S, T> {
+
+    /**
+     * Translates a source value into a target value.
+     * 
+     * @param source the source value to be translated
+     * @return the target value
+     * @throws IOException in case that translation fails
+     */
+    public T to(S source) throws IOException;
+    
+}
