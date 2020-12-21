e591d599978b11856145ee29f0b52646e522f90d
eichelberger <eichelberger@sse.uni-hildesheim.de>
2020-12-21 15:20:56 +0100

    disabled challenging test

--- platform/support/support.aas.basyx/src/test/java/test/de/iip_ecosphere/platform/support/aas/basyx/DeploymentTest.java
+++ platform/support/support.aas.basyx/src/test/java/test/de/iip_ecosphere/platform/support/aas/basyx/DeploymentTest.java
@@ -13,8 +13,10 @@
 package test.de.iip_ecosphere.platform.support.aas.basyx;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import de.iip_ecosphere.platform.support.NetUtils;
@@ -22,8 +24,12 @@ import de.iip_ecosphere.platform.support.aas.Aas;
 import de.iip_ecosphere.platform.support.aas.Aas.AasBuilder;
 import de.iip_ecosphere.platform.support.aas.AasFactory;
 import de.iip_ecosphere.platform.support.aas.AasServer;
+import de.iip_ecosphere.platform.support.aas.Property;
 import de.iip_ecosphere.platform.support.aas.Submodel;
 import de.iip_ecosphere.platform.support.aas.Submodel.SubmodelBuilder;
+import de.iip_ecosphere.platform.support.aas.SubmodelElementCollection;
+import de.iip_ecosphere.platform.support.aas.SubmodelElementCollection.SubmodelElementCollectionBuilder;
+import de.iip_ecosphere.platform.support.aas.Type;
 
 /**
  * Tests deployment scenarios.
@@ -62,6 +68,65 @@ public class DeploymentTest {
         Assert.assertNotNull(aas.getSubmodel("dynamic"));
 
         server.stop();
+    }
+    
+    /**
+     * Tests a changed attribute value on a dynamically deployed sub-model elements collection. Does not work,
+     * similarly when creating the connectors component AAS.
+     * 
+     * @throws IOException shall not occur
+     * @throws ExecutionException shall not occur
+     */
+    @Ignore("Fails setting value with ResourceNotFoundException although there")
+    @Test
+    public void localDynamicSubmodelElementsCollectionPropertyDeployment() throws IOException, ExecutionException {
+        final String urn = "urn:::AAS:::testMachines#";
+        final String host = "localhost";
+        final int port = NetUtils.getEphemeralPort();
+        final String registryPath = "registry";
+        
+        AasFactory factory = AasFactory.getInstance();
+        AasBuilder aasB = factory.createAasBuilder("myAas", urn);
+        SubmodelBuilder smB = aasB.createSubmodelBuilder("sub");
+        smB.build();
+        Aas aas = aasB.build();
+        
+        AasServer server = factory.createDeploymentRecipe(host, port)
+            .addInMemoryRegistry(registryPath)
+            .deploy(aas)
+            .createServer();
+        server.start(2000);
+
+        aas = factory.retrieveAas(host, port, registryPath, urn);
+        Submodel sub = aas.getSubmodel("sub");
+        Assert.assertNotNull(sub);
+        SubmodelElementCollectionBuilder smcB = sub.addSubmodelElementCollection("coll", false, false);
+        smcB.createPropertyBuilder("prop").setValue(Type.BOOLEAN, true).build();
+        smcB.build();
+
+        aas = factory.retrieveAas(host, port, registryPath, urn);
+        sub = aas.getSubmodel("sub");
+        Assert.assertNotNull(sub);
+        SubmodelElementCollection coll = sub.getSubmodelElementCollection("coll");
+        Assert.assertNotNull(coll);
+        Property prop = coll.getProperty("prop");
+        Assert.assertNotNull(prop);
+        Assert.assertEquals(true, prop.getValue());
+
+        prop.setValue(false);
+        Assert.assertEquals(false, prop.getValue());
+
+        aas = factory.retrieveAas(host, port, registryPath, urn);
+        sub = aas.getSubmodel("sub");
+        Assert.assertNotNull(sub);
+        coll = sub.getSubmodelElementCollection("coll");
+        Assert.assertNotNull(coll);
+        prop = coll.getProperty("prop");
+        Assert.assertNotNull(prop);
+        Assert.assertEquals(false, prop.getValue());
+
+        server.stop();
+
     }
 
 }
