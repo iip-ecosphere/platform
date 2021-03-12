e73c7f6d234fcad46eeb5351c110c8246822aabe
eichelberger <eichelberger@sse.uni-hildesheim.de>
2021-03-12 16:24:56 +0100

    removing migrated methods

--- platform/transport/transport/src/test/java/test/de/iip_ecosphere/platform/transport/AbstractTestServer.java
+++ platform/transport/transport/src/test/java/test/de/iip_ecosphere/platform/transport/AbstractTestServer.java
@@ -15,14 +15,8 @@ package test.de.iip_ecosphere.platform.transport;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
-import org.apache.commons.io.FileUtils;
-
+import de.iip_ecosphere.platform.support.JarUtils;
 import de.iip_ecosphere.platform.support.Server;
 
 /**
@@ -33,34 +27,6 @@ import de.iip_ecosphere.platform.support.Server;
 public abstract class AbstractTestServer implements Server {
 
     private static File configDir;
-
-    /**
-     * Creates a temporary folder without cleanup.
-     * 
-     * @param name the name of the temporary folder within the system/user temporary directory
-     * @return the temporary folder (descriptor)
-     */
-    public static File createTmpFolder(String name) {
-        return createTmpFolder(name, false);
-    }
-
-    /**
-     * Creates a temporary folder.
-     * 
-     * @param name the name of the temporary folder within the system/user temporary directory
-     * @param cleanup try to do an auto cleanup at JVM shutdown
-     * @return the temporary folder (descriptor)
-     */
-    public static File createTmpFolder(String name, boolean cleanup) {
-        String tmp = System.getProperty("java.io.tmpdir");
-        File result = new File(tmp, name);
-        FileUtils.deleteQuietly(result);
-        result.mkdir();
-        if (cleanup) {
-            result.deleteOnExit();
-        }
-        return result;
-    }
 
     /**
      * Defines the server configuration directory.
@@ -119,7 +85,7 @@ public abstract class AbstractTestServer implements Server {
         cfgDir.mkdirs();
         InputStream in = loader.getResourceAsStream(location);
         if (null != in) {
-            extractZip(in, cfgDir.toPath());
+            JarUtils.extractZip(in, cfgDir.toPath());
         } else {
             throw new IOException("Location '" + location + "' cannot be found");
         }
@@ -134,60 +100,6 @@ public abstract class AbstractTestServer implements Server {
         String intlName = AbstractTestServer.class.getName().replace('.', '/');
         String classJar = AbstractTestServer.class.getResource("/" + intlName + ".class").toString();
         return classJar.startsWith("jar:");
-    }
-
-    // adapted from https://mkyong.com/java/how-to-decompress-files-from-a-zip-file/
-    // may be worth moving down...
-    
-    /**
-     * Extracts a ZIP file.
-     * 
-     * @param in the input stream containing the ZIP file
-     * @param target the target path
-     * @throws IOException if something I/O related fails
-     */
-    public static void extractZip(InputStream in, Path target) throws IOException {
-        try (ZipInputStream zis = new ZipInputStream(in)) {
-            ZipEntry zipEntry = zis.getNextEntry();
-            while (zipEntry != null) {
-                boolean isDirectory = zipEntry.isDirectory();
-                Path newPath = zipSlipProtect(zipEntry, target);
-                if (isDirectory) {
-                    Files.createDirectories(newPath);
-                } else {
-                    if (newPath.getParent() != null) {
-                        if (Files.notExists(newPath.getParent())) {
-                            Files.createDirectories(newPath.getParent());
-                        }
-                    }
-                    Files.copy(zis, newPath, StandardCopyOption.REPLACE_EXISTING);
-                }
-                zipEntry = zis.getNextEntry();
-            }
-            zis.closeEntry();
-        } catch (IOException e) {
-            throw e;
-        }
-    }
-    
-    /**
-     * Protects from ZIP slip attack.
-     * 
-     * @param zipEntry the ZIP entry
-     * @param targetDir the target directory
-     * @return the normalized/fixed path
-     * @throws IOException if something I/O related fails
-     */
-    private static Path zipSlipProtect(ZipEntry zipEntry, Path targetDir)
-        throws IOException {
-        Path targetDirResolved = targetDir.resolve(zipEntry.getName());
-        // make sure normalized file still has targetDir as its prefix
-        // else throws exception
-        Path normalizePath = targetDirResolved.normalize();
-        if (!normalizePath.startsWith(targetDir)) {
-            throw new IOException("Bad zip entry: " + zipEntry.getName());
-        }
-        return normalizePath;
     }
     
 }
