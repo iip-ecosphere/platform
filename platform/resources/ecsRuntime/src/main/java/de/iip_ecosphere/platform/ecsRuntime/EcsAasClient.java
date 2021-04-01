ee9bb55875e014c6dd9fb45e138d29c1ba2cb6b4
eichelberger <eichelberger@sse.uni-hildesheim.de>
2021-04-01 15:02:52 +0200

    ups... missing files

---
+++ platform/resources/ecsRuntime/src/main/java/de/iip_ecosphere/platform/ecsRuntime/EcsAasClient.java
@@ -0,0 +1,122 @@
+/**
+ * ******************************************************************************
+ * Copyright (c) {2021} The original author or authors
+ *
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License 2.0 which is available 
+ * at http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR EPL-2.0
+ ********************************************************************************/
+
+package de.iip_ecosphere.platform.ecsRuntime;
+
+import static de.iip_ecosphere.platform.support.iip_aas.json.JsonResultWrapper.fromJson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.concurrent.ExecutionException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.iip_ecosphere.platform.support.aas.Submodel;
+import de.iip_ecosphere.platform.support.iip_aas.AasPartRegistry;
+import de.iip_ecosphere.platform.support.iip_aas.ActiveAasBase;
+import de.iip_ecosphere.platform.support.iip_aas.NetworkManagerAas;
+import de.iip_ecosphere.platform.support.iip_aas.SubmodelClient;
+
+/**
+ * A client for {@link EcsAas}.
+ * 
+ * @author Holger Eichelberger, SSE
+ */
+public class EcsAasClient extends SubmodelClient implements ContainerOperations {
+
+    /**
+     * Creates a client instance based on a deployed IIP-AAS from {@link AasPartRegistry} based on a submodel with
+     * {@link NetworkManagerAas#NAME_SUBMODEL name}.
+     * 
+     * @throws IOException if retrieving the IIP-AAS or the respective submodel fails
+     */
+    public EcsAasClient() throws IOException {
+        this(ActiveAasBase.getSubmodel(EcsAas.NAME_SUBMODEL));
+    }
+    
+    /**
+     * Creates a client instance based on the submodel. The submodel shall conform to {@link EcsAas} with 
+     * respect to the operations, signatures, but also the {@link EcsAas#NAME_SUBMODEL name},
+     * 
+     * @param submodel the submodel to use
+     */
+    public EcsAasClient(Submodel submodel) {
+        super(submodel);
+    }
+
+    @Override
+    public String addContainer(URI location) throws ExecutionException {
+        return fromJson(getOperation(EcsAas.NAME_OP_CONTAINER_ADD).invoke(location.toString()));
+    }
+
+    @Override
+    public void startContainer(String id) throws ExecutionException {
+        fromJson(getOperation(EcsAas.NAME_OP_CONTAINER_START).invoke(id));
+    }
+
+    @Override
+    public void stopContainer(String id) throws ExecutionException {
+        fromJson(getOperation(EcsAas.NAME_OP_CONTAINER_STOP).invoke(id));
+    }
+
+    @Override
+    public void migrateContainer(String id, URI location) throws ExecutionException {
+        fromJson(getOperation(EcsAas.NAME_OP_CONTAINER_MIGRATE).invoke(id, location.toString()));
+    }
+
+    @Override
+    public void undeployContainer(String id) throws ExecutionException {
+        fromJson(getOperation(EcsAas.NAME_OP_CONTAINER_UNDEPLOY).invoke(id));
+    }
+
+    @Override
+    public void updateContainer(String id, URI location) throws ExecutionException {
+        fromJson(getOperation(EcsAas.NAME_OP_CONTAINER_UPDATE).invoke(id, location.toString()));
+    }
+
+    @Override
+    public ContainerState getState(String id) {
+        ContainerState result = ContainerState.UNKNOWN;
+        try {
+            String tmp = fromJson(getOperation(EcsAas.NAME_OP_GET_STATE).invoke(id));
+            if (null != tmp) {
+                result = ContainerState.valueOf(tmp);
+            }
+        } catch (ExecutionException e) {
+            getLogger().error("Requesting service state: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            getLogger().error("Requesting service state, illegal response value: " + e.getMessage());
+        }
+        return result;
+    }
+
+    /**
+     * Returns the logger instance.
+     * 
+     * @return the logger
+     */
+    private Logger getLogger() {
+        return LoggerFactory.getLogger(getClass());
+    }
+
+    @Override
+    public String getContainerSystemName() {
+        return getPropertyStringValue(EcsAas.NAME_PROP_CSYS_NAME, "");
+    }
+
+    @Override
+    public String getContainerSystemVersion() {
+        return getPropertyStringValue(EcsAas.NAME_PROP_CSYS_VERSION, "");
+    }
+
+}
