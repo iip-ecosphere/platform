5537276e357c1d632c135689b38ec901e095d862
Monika <you@example.com>
2021-04-27 17:36:57 +0200

    removed unnecessary attributes

--- platform/resources/ecsRuntime.docker/src/main/java/de/iip_ecosphere/platform/ecsRuntime/docker/DockerConfiguration.java
+++ platform/resources/ecsRuntime.docker/src/main/java/de/iip_ecosphere/platform/ecsRuntime/docker/DockerConfiguration.java
@@ -17,7 +17,6 @@ import java.io.IOException;
 import org.slf4j.LoggerFactory;
 
 import de.iip_ecosphere.platform.ecsRuntime.Configuration;
-import de.iip_ecosphere.platform.support.iip_aas.Version;
 
 /**
  * Implements the docker specific configuration. For configuration prerequisites, see {@link Configuration}.
@@ -27,9 +26,6 @@ import de.iip_ecosphere.platform.support.iip_aas.Version;
 public class DockerConfiguration extends Configuration {
 
     private String dockerHost;
-    private int id;
-    private String name;
-    private Version version;
    
     /**
      * Returns the docker host.
@@ -49,54 +45,6 @@ public class DockerConfiguration extends Configuration {
         this.dockerHost = dockerHost;
     }
     
-    /**
-     * Returns the container's id.
-     * @return id
-     */
-    public int getId() {
-        return id;
-    }
-
-    /**
-     * Defines the container's id.
-     * @param id the container's id
-     */
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    /**
-     * Returns the container's name.
-     * @return name 
-     */
-    public String getName() {
-        return name;
-    }
-    
-    /**
-     * Defines the container's name.
-     * @param name
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * Returns the container's version.
-     * @return version
-     */
-    public Version getVersion() {
-        return version;
-    }
-
-    /**
-     * Defines the container's version.
-     * @param version
-     */
-    public void setVersion(Version version) {
-        this.version = version;
-    }
-
      /**
      * Reads a {@link DockerConfiguration} instance from a default "ecsRuntime.yml" file in the root folder of the jar. 
      *
