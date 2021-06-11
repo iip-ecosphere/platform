df39c5391ef99bca1d09ee4ae69e8cdd411ecea7
eichelberger <eichelberger@sse.uni-hildesheim.de>
2021-06-11 11:34:35 +0200

    some logging

--- platform/services/services.environment/src/main/java/de/iip_ecosphere/platform/services/environment/Starter.java
+++ platform/services/services.environment/src/main/java/de/iip_ecosphere/platform/services/environment/Starter.java
@@ -1,10 +1,29 @@
+/**
+ * ******************************************************************************
+ * Copyright (c) {2021} The original author or authors
+ *
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License 2.0 which is available 
+ * at http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR EPL-2.0
+ ********************************************************************************/
+
 package de.iip_ecosphere.platform.services.environment;
+
+import org.slf4j.LoggerFactory;
 
 import de.iip_ecosphere.platform.support.NetUtils;
 import de.iip_ecosphere.platform.support.Server;
 import de.iip_ecosphere.platform.support.aas.AasFactory;
 import de.iip_ecosphere.platform.support.aas.ProtocolServerBuilder;
 
+/**
+ * Service environment starter reading command server information from the command line.
+ * 
+ * @author Holger Eichelberger, SSE
+ */
 public class Starter {
     
     public static final String PARAM_IIP_PROTOCOL = "iip.protocol";
@@ -36,6 +55,9 @@ public class Starter {
         if (!found) {
             protocol = AasFactory.DEFAULT_PROTOCOL;
         }
+
+        LoggerFactory.getLogger(Starter.class).info("Configuring service command server for protocol '" + protocol 
+            + "' (empty means default) and port " + port);
         builder = factory.createProtocolServerBuilder(protocol, port);
     }
     
@@ -43,6 +65,7 @@ public class Starter {
      * Starts the server instance(s).
      */
     public static void start() {
+        LoggerFactory.getLogger(Starter.class).info("Starting service command server");
         server = builder.build();
         server.start();
     }
