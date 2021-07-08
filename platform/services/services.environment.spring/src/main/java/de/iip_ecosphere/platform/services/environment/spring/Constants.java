c7e560996fe93c0e2a852f8534a8d802d25f6fa6
eichelberger <eichelberger@sse.uni-hildesheim.de>
2021-07-08 17:48:56 +0200

    the IIP mimetype for serialization

---
+++ platform/services/services.environment.spring/src/main/java/de/iip_ecosphere/platform/services/environment/spring/Constants.java
@@ -0,0 +1,29 @@
+/**
+ * ******************************************************************************
+ * Copyright (c) {2021} The original author or authors
+ *
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License 2.0 which is available 
+ * at http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR EPL-2.0
+ ********************************************************************************/
+
+package de.iip_ecosphere.platform.services.environment.spring;
+
+import org.springframework.util.MimeType;
+
+/**
+ * Defines global spring-related constants.
+ * 
+ * @author Holger Eichelberger, SSE
+ */
+public class Constants {
+
+    /**
+     * The default mime type for IIP-Ecosphere serialized data types via the transport layer.
+     */
+    public static final MimeType MIME_TYPE = new MimeType("application", "iip");
+
+}
