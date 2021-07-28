097b8c26a1e14afc7da0070ad26d79804a7306e8
eichelberger <eichelberger@sse.uni-hildesheim.de>
2021-07-28 08:12:51 +0200

    default string serializer test

--- platform/transport/transport/src/test/java/test/de/iip_ecosphere/platform/transport/SerializerRegistryTest.java
+++ platform/transport/transport/src/test/java/test/de/iip_ecosphere/platform/transport/SerializerRegistryTest.java
@@ -2,7 +2,11 @@ package test.de.iip_ecosphere.platform.transport;
 
 import org.junit.Test;
 
+import de.iip_ecosphere.platform.transport.serialization.Serializer;
 import de.iip_ecosphere.platform.transport.serialization.SerializerRegistry;
+
+import java.io.IOException;
+
 import org.junit.Assert;
 
 /**
@@ -16,12 +20,19 @@ public class SerializerRegistryTest {
      * Tests setting/reading the registry name.
      */
     @Test
-    public void testName() {
+    public void testName() throws IOException {
         final String name = "XXX";
         String old = SerializerRegistry.setName(name);
         Assert.assertEquals(name, SerializerRegistry.getName());
         SerializerRegistry.setName(old);
         SerializerRegistry.resetDefaults();
+        
+        Serializer<String> ser = SerializerRegistry.getSerializer(String.class);
+        Assert.assertNotNull(ser);
+        String test = "ABBA";
+        byte[] data = ser.to(test);
+        String tmp = ser.from(data);
+        Assert.assertEquals(test, tmp);
     }
     
 }
