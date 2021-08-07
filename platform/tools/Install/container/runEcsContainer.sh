fa74bca7fc1762ceae0157f47d1ccb0c1aef246c
eichelberger <eichelberger@sse.uni-hildesheim.de>
2021-08-07 11:18:36 +0200

    mapping the app into the ECS container as volume

--- platform/tools/Install/container/runEcsContainer.sh
+++ platform/tools/Install/container/runEcsContainer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -v /var/run/docker.sock:/var/run/docker.sock -P --network=host -it iip/ecsruntime:0.2
+docker run -v /var/run/docker.sock:/var/run/docker.sock -P --network=host --mount type=bind,source="$(pwd)"/SimpleMeshTestingApp,target=/SimpleMeshTestingApp -it iip/ecsruntime:0.2
---
+++ platform/tools/Install/container/saveEcsContainer.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker save iip/ecsruntime:0.2 | gzip > EcsRuntime/ecsruntime-0.2.tar.gz
