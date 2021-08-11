0a4d78eb991f566ee4aa38f80947c641f005cf65
Monika <you@example.com>
2021-08-11 15:17:44 +0200

    syntax error

--- platform/services/services.environment/src/main/python/Starter.py
+++ platform/services/services.environment/src/main/python/Starter.py
@@ -1,15 +1,15 @@
 import logging as logger
+
 logger.basicConfig(level="DEBUG")
 import argparse
-
 from BaSyxTCPServer import BaSyxTCPServer
-from BaSyxHTTPServer import BaSyxHTTPServer
+
 from VabIipOperationsBuilder import VabIipOperationsBuilder
 from ServiceMapper import mapService
 
 
 def start(services):
-    """Starts the Python Java framework for the given instances of administrative service wrappers.
+    """Starts the Python Java framework for the given instances of administratove service wrappers.
 
     Parameters:
       services -- list of Service instances
@@ -27,17 +27,11 @@ def start(services):
         mapService(builder, service)
 
     port = args.port[0]
-    protocol = args.protocol[0]
-
-    if protocol == "" or protocol == "VAB-TCP":
-        print('Protocol: ' + protocol)
+    if args.protocol == "" or args.protocol == "VAB-TCP":
         server = BaSyxTCPServer(builder, port)
         # other protocols would go into here
-    elif protocol == "VAB-HTTP":
-        server = BaSyxHTTPServer(port)
     else:
-        logger.info("Protocol '" + protocol + "' unknown. Using default.")
-        logger.info('Protocol %s', protocol)
+        logger.info("Protocol '" + args.protocol + "' unknown. Using default.");
         server = BaSyxTCPServer(builder, port)
 
     server.start()
