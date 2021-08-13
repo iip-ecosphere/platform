057684f3f0a5369b88b4c88a3cd6335d3368578a
eichelberger <eichelberger@sse.uni-hildesheim.de>
2021-08-13 15:57:48 +0200

    updates release scripts, basyx fragment

--- platform/tools/MvnCentral/deploy.bat
+++ platform/tools/MvnCentral/deploy.bat
@@ -4,11 +4,12 @@ REM install maven into PATH
 REM install GnuPGP
 REM add your account settings for ossrh into maven setup (use authentication token)
 REM copy this script into an empty directory
-REM run this script
+REM run mvn package
+REM run this script (once for basix, once for platform with different version numbers if needed)
 REM goto https://oss.sonatype.org/#welcome, staging repositories, deiip-ecosphere...*, close for check/deploy
 
 setlocal ENABLEDELAYEDEXPANSION
-SET VERSION=0.1.0
+SET VERSION=0.2.0
 SET TARGET=https://oss.sonatype.org/service/local/staging/deploy/maven2
 SET REPO=ossrh-iip
 SET DEPLOYCMD=mvn gpg:sign-and-deploy-file -Durl=%TARGET% -DrepositoryId=%REPO%
@@ -38,14 +39,41 @@ REM param2: version of the artifact to deploy
     SET TESTSOURCES=%ARTIFACTPREFIX%-test-sources.jar
     SET JAVADOC=%ARTIFACTPREFIX%-javadoc.jar
     SET TESTJAVADOC=%ARTIFACTPREFIX%-test-javadoc.jar
+    SET PYTHON=%ARTIFACTPREFIX%-python.jar
+    SET EASY=%ARTIFACTPREFIX%-easy.jar
     
     REM deploy to central
-    call %DEPLOYCMD% -DpomFile=%FOLDER%\%POM% -Dfile=%FOLDER%\%JAR%
-    call %DEPLOYCMD% -DpomFile=%FOLDER%\%POM% -Dfile=%FOLDER%\%TESTS% -Dclassifier=tests
-    call %DEPLOYCMD% -DpomFile=%FOLDER%\%POM% -Dfile=%FOLDER%\%SOURCES% -Dclassifier=sources
-    call %DEPLOYCMD% -DpomFile=%FOLDER%\%POM% -Dfile=%FOLDER%\%TESTSOURCES% -Dclassifier=test-sources
-    call %DEPLOYCMD% -DpomFile=%FOLDER%\%POM% -Dfile=%FOLDER%\%JAVADOC% -Dclassifier=javadoc
-    call %DEPLOYCMD% -DpomFile=%FOLDER%\%POM% -Dfile=%FOLDER%\%TESTJAVADOC% -Dclassifier=test-javadoc
+    if EXIST %FOLDER%\%JAR% (
+        echo "JAR %FOLDER%\%JAR%"
+        call %DEPLOYCMD% -DpomFile=%FOLDER%\%POM% -Dfile=%FOLDER%\%JAR%
+        echo "TESTS %FOLDER%\%TESTS%"
+        call %DEPLOYCMD% -DpomFile=%FOLDER%\%POM% -Dfile=%FOLDER%\%TESTS% -Dclassifier=tests
+        echo "SOURCES %FOLDER%\%SOURCES%"
+        call %DEPLOYCMD% -DpomFile=%FOLDER%\%POM% -Dfile=%FOLDER%\%SOURCES% -Dclassifier=sources
+        if EXIST %FOLDER%\%TESTSOURCES% (
+          echo "TEST-SOURCES %FOLDER%\%TESTSOURCES%"
+          call %DEPLOYCMD% -DpomFile=%FOLDER%\%POM% -Dfile=%FOLDER%\%TESTSOURCES% -Dclassifier=test-sources
+        )
+        if EXIST %FOLDER%\%JAVADOC% (
+          echo "JAVADOC %FOLDER%\%JAVADOC%"
+          call %DEPLOYCMD% -DpomFile=%FOLDER%\%POM% -Dfile=%FOLDER%\%JAVADOC% -Dclassifier=javadoc
+        )
+        if EXIST %FOLDER%\%TESTJAVADOC% (
+          echo "TEST-JAVADOC %FOLDER%\%TESTJAVADOC%"
+          call %DEPLOYCMD% -DpomFile=%FOLDER%\%POM% -Dfile=%FOLDER%\%TESTJAVADOC% -Dclassifier=test-javadoc
+        )
+        if EXIST %FOLDER%\%PYTHON% (
+          echo "PYTHON %FOLDER%\%PYTHON%"
+          call %DEPLOYCMD% -DpomFile=%FOLDER%\%POM% -Dfile=%FOLDER%\%PYTHON% -Dclassifier=python
+        )
+        if EXIST %FOLDER%\%EASY% (
+          echo "EASY %FOLDER%\%EASY%"
+          call %DEPLOYCMD% -DpomFile=%FOLDER%\%POM% -Dfile=%FOLDER%\%EASY% -Dclassifier=easy
+        )
+    ) else (
+        echo "POM %FOLDER%\%POM%"
+        call %DEPLOYCMD% -DpomFile=%FOLDER%\%POM% -Dfile=%FOLDER%\%POM%
+    )
 
     endlocal
     goto :eof
