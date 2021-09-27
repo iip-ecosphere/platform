d2d763b6284d68e5ebf98a4ee850477f64db4c4c
eichelberger <eichelberger@sse.uni-hildesheim.de>
2021-09-27 12:21:03 +0200

    Initial steps towards kodex integration.

---
+++ platform/securityDataProtection/security.services.kodex/src/main/java/de/iip_ecosphere/platform/security/services/kodex/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Implementation.
+ */
+package de.iip_ecosphere.platform.security.services.kodex;
---
+++ platform/securityDataProtection/security.services.kodex/src/test/java/test/de/iip_ecosphere/platform/security/services/kodex/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Tests for the ECS runtime.
+ */
+package test.de.iip_ecosphere.platform.security.services.kodex;
