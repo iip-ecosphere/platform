2eef7140b59fec3b9a2c6a2be4e477deb1ecaaba
eichelberger <eichelberger@sse.uni-hildesheim.de>
2021-10-26 10:17:13 +0200

    removing log-output

--- platform/transport/transport.spring.mqttv5/src/test/java/test/de/iip_ecosphere/platform/transport/spring/binder/mqttv5/MqttV5MessageBinderTest.java
+++ platform/transport/transport.spring.mqttv5/src/test/java/test/de/iip_ecosphere/platform/transport/spring/binder/mqttv5/MqttV5MessageBinderTest.java
@@ -154,7 +154,6 @@ public class MqttV5MessageBinderTest {
     
                 @Override
                 public void received(String data) {
-                    System.out.println("Infra received " + data);
                     try {
                         infra.asyncSend("input2", "config " + data);
                     } catch (IOException e) {
@@ -235,7 +234,6 @@ public class MqttV5MessageBinderTest {
         @Bean
         public Function<String, String> transform() {
             return in -> {
-                System.out.println("TRANSFORM " + in);                
                 return in + " world";
             };
         }
@@ -248,7 +246,6 @@ public class MqttV5MessageBinderTest {
         @Bean
         public Consumer<String> receiveInput() {
             return s -> {
-                System.out.println("RECEIVED " + s);
                 received = s;
             };
         }
