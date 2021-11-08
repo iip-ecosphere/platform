b6e7e398b5de491324cad8cef339af90f4a0d893
eichelberger <eichelberger@sse.uni-hildesheim.de>
2021-11-08 21:51:22 +0100

    javadoc

--- platform/resources/deviceMgt/src/main/java/de/iip_ecosphere/platform/deviceMgt/DeviceManagementClient.java
+++ platform/resources/deviceMgt/src/main/java/de/iip_ecosphere/platform/deviceMgt/DeviceManagementClient.java
@@ -1,5 +1,22 @@
+/**
+ * ******************************************************************************
+ * Copyright (c) {2021} The original author or authors
+ *
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License 2.0 which is available 
+ * at http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR EPL-2.0
+ ********************************************************************************/
+
 package de.iip_ecosphere.platform.deviceMgt;
 
+/**
+ * A device management client joins all operation interfaces.
+ * 
+ * @author Holger Eichelberger, SSE
+ */
 public interface DeviceManagementClient extends DeviceFirmwareOperations, DeviceRemoteManagementOperations, 
     DeviceResourceConfigOperations {
 
