3232473c29b71ea3a8ceb745fabb61c7d53d476b
eichelberger <eichelberger@sse.uni-hildesheim.de>
2021-12-06 15:01:21 +0100

    parallel start of platform parts?

--- platform/tools/Install/container/fullPlatform/platform/wrapper_script.sh
+++ platform/tools/Install/container/fullPlatform/platform/wrapper_script.sh
@@ -2,8 +2,25 @@
   
 # turn on bash's job control
 set -m
+echo "Starting AMQP broker..."
 ./amqp.sh &
+echo "Starting platform server..."
 ./platform.sh &
+while [ ! -f /run/iip-platform.pid ]
+do
+  sleep 1
+done
+echo "Starting local ECS-Runtime..."
 ./ecs.sh &
+while [ ! -f /run/iip-ecsRuntime.pid ]
+do
+  sleep 1
+done
+echo "Starting local Service Manager..."
 ./serviceMgr.sh
+while [ ! -f /run/iip-serviceMgr.pid ]
+do
+  sleep 1
+done
+
 fg %1
