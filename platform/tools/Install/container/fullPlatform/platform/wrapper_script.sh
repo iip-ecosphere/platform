ae0379d38f6eb9d48d30705fd6f32edf5395e3e8
eichelberger <eichelberger@sse.uni-hildesheim.de>
2021-12-07 13:48:08 +0100

    just some markers

--- platform/tools/Install/container/fullPlatform/platform/wrapper_script.sh
+++ platform/tools/Install/container/fullPlatform/platform/wrapper_script.sh
@@ -2,21 +2,21 @@
   
 # turn on bash's job control
 set -m
-echo "Starting AMQP broker..."
+echo ">>> Starting AMQP broker..."
 ./amqp.sh &
-echo "Starting platform server..."
+echo ">>> Starting platform server..."
 ./platform.sh &
 while [ ! -f /run/iip-platform.pid ]
 do
   sleep 1
 done
-echo "Starting local ECS-Runtime..."
+echo ">>> Starting local ECS-Runtime..."
 ./ecs.sh &
 while [ ! -f /run/iip-ecsRuntime.pid ]
 do
   sleep 1
 done
-echo "Starting local Service Manager..."
+echo ">>> Starting local Service Manager..."
 ./serviceMgr.sh
 while [ ! -f /run/iip-serviceMgr.pid ]
 do
