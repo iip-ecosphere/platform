f3d070bd11a4942c3b9907d8407c9ab46b0710c4
eichelbe <eichelberger@sse.uni-hildesheim.de>
2022-02-21 09:09:08 +0100

    dynamic creation of type translators/serializers

--- platform/transport/transport/src/main/java/de/iip_ecosphere/platform/transport/serialization/TypeTranslators.java
+++ platform/transport/transport/src/main/java/de/iip_ecosphere/platform/transport/serialization/TypeTranslators.java
@@ -15,6 +15,7 @@ package de.iip_ecosphere.platform.transport.serialization;
 import java.io.IOException;
 
 import org.apache.commons.text.StringEscapeUtils;
+import org.slf4j.LoggerFactory;
 
 /**
  * Defines a set of type translators for primitive types.
@@ -136,5 +137,44 @@ public class TypeTranslators {
         }
         
     };
+    
+    /**
+     * Convenience method for creating (custom) type translator instances.
+     * 
+     * @param loader the class loader to load the class with
+     * @param className the name of the type translator class (must provide a no-argument constructor)
+     * @return the type translator instance (or <b>null</b> if the translator cannot be found/initialized)
+     */
+    public static TypeTranslator<?, ?> createTypeTranslator(ClassLoader loader, String className) {
+        TypeTranslator<?, ?> result = null;
+        try {
+            Class<?> translatorClass = loader.loadClass(className);
+            result = (TypeTranslator<?, ?>) translatorClass.newInstance();
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | ClassCastException e) {
+            String loaders = "";
+            ClassLoader l = loader;
+            while (null != l) {
+                if (loaders.length() > 0) {
+                    loaders += " -> ";
+                }
+                loaders += l.getClass().getSimpleName();
+                l = l.getParent();
+            }
+            LoggerFactory.getLogger(TypeTranslators.class).error("Cannot instantiate instance of type '" 
+                + className + " via " + loaders + "': " + e.getClass().getSimpleName() + " " + e.getMessage());
+        }
+        return result;
+    }
+
+    /**
+     * Convenience method for creating (custom) serializer instances.
+     * 
+     * @param loader the class loader to load the class with
+     * @param className the name of the type serializer class (must provide a no-argument constructor)
+     * @return the type serializer instance (or <b>null</b> if the serializer cannot be found/initialized)
+     */
+    public static Serializer<?> createSerializer(ClassLoader loader, String className) {
+        return (Serializer<?>) createTypeTranslator(loader, className);
+    }
 
 }
