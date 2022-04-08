249da9526f21069d42e9e78eb5a52fbd4fc12df2
eichelbe <eichelberger@sse.uni-hildesheim.de>
2022-04-08 16:55:23 +0200

    build script fix

--- platform/examples/examples.rtsa/build.sh
+++ platform/examples/examples.rtsa/build.sh
@@ -3,7 +3,7 @@
 #Explanation see README.MD
 
 mvn -U -f pom-model.xml generate-sources
-mvn -f pom-model.xml exec:java -Dexec.args="ExamplePython src/test/easy gen/py generateAppsNoDeps" -Diip.resources="$PWD/resources"
+mvn -f pom-model.xml exec:java -Dexec.args="ExampleRTSA src/test/easy gen/rtsa generateAppsNoDeps" -Diip.resources="$PWD/resources"
 mvn -U install -DskipTests
-mvn -f pom-model.xml exec:java -Dexec.args="ExamplePython src/test/easy gen/py generateApps" -Diip.resources="$PWD/resources"
+mvn -f pom-model.xml exec:java -Dexec.args="ExampleRTSA src/test/easy gen/rtsa generateApps" -Diip.resources="$PWD/resources"
 #mvn exec:java
