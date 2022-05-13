c9fd66a599534b7b487df6d07b453287268e984c
eichelbe <eichelberger@sse.uni-hildesheim.de>
2022-05-13 17:09:10 +0200

    Build with latest

--- platform/managementUI/build.sh
+++ platform/managementUI/build.sh
@@ -7,4 +7,6 @@ ng build --base-href ./
 mkdir pckg
 timestamp=$(date +%Y%m%d%H%M%S)
 zip -r pckg/IIP-Ecosphere-mgtUi-$timestamp.zip dist
+cp pckg/IIP-Ecosphere-mgtUi-$timestamp.zip pckg/IIP-Ecosphere-mgtUi-latest.zip
 tar czvf pckg/IIP-Ecosphere-mgtUi-$timestamp.tgz dist
+cp pckg/IIP-Ecosphere-mgtUi-$timestamp.tgz IIP-Ecosphere-mgtUi-latest.tgz
