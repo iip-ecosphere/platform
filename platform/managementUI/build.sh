0c813674b57041608dd735a89004abeeb39cd930
eichelbe <eichelberger@sse.uni-hildesheim.de>
2022-05-14 13:00:31 +0200

    local test works, let's see for CI

--- platform/managementUI/build.sh
+++ platform/managementUI/build.sh
@@ -1,6 +1,7 @@
-#Build script for CI
-#Assumption npm and angular cli installed
 #!/bin/bash
+
+# Build script for CI
+# Assumption npm and angular cli installed
 
 npm install
 ng build --base-href ./
@@ -10,3 +11,8 @@ zip -r pckg/IIP-Ecosphere-mgtUi-$timestamp.zip dist
 cp pckg/IIP-Ecosphere-mgtUi-$timestamp.zip pckg/IIP-Ecosphere-mgtUi-latest.zip
 tar czvf pckg/IIP-Ecosphere-mgtUi-$timestamp.tgz dist
 cp pckg/IIP-Ecosphere-mgtUi-$timestamp.tgz pckg/IIP-Ecosphere-mgtUi-latest.tgz
+
+# run pseudo maven and deploy to SSE maven repo
+# maven copies pckg/IIP-Ecosphere-mgtUi-latest.zip to target and renames it as Maven knows the version/project name
+# ANT uses SSE macros to call maven and to deploy file in target
+ant -f build-jk.xml
