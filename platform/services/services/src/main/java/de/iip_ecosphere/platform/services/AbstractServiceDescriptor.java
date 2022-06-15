e61554ce7b3f67fa7bd27d88e47c25304dae51fb
eichelbe <eichelberger@sse.uni-hildesheim.de>
2022-06-15 08:42:19 +0200

    taking up implicit state change

--- platform/services/services/src/main/java/de/iip_ecosphere/platform/services/AbstractServiceDescriptor.java
+++ platform/services/services/src/main/java/de/iip_ecosphere/platform/services/AbstractServiceDescriptor.java
@@ -154,6 +154,7 @@ public abstract class AbstractServiceDescriptor<A extends ArtifactDescriptor> im
         if (null != stub) {
             try {            
                 stub.setState(state);
+                state = stub.getState(); // may do a transition
             } catch (ExecutionException e) {
                 // may fail, keep local; handover needed
                 LoggerFactory.getLogger(getClass()).info("Cannot set state for service '" + getId() + "' via AAS. "
