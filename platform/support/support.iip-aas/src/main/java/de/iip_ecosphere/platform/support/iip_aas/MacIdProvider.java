52609df62e05fbc78c014650498e02b205d7b2e2
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2022-08-10 23:25:10 +0200

    a bit more determinism for Ubuntu/Docker

--- platform/support/support.iip-aas/src/main/java/de/iip_ecosphere/platform/support/iip_aas/MacIdProvider.java
+++ platform/support/support.iip-aas/src/main/java/de/iip_ecosphere/platform/support/iip_aas/MacIdProvider.java
@@ -15,7 +15,10 @@ package de.iip_ecosphere.platform.support.iip_aas;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
+import java.util.List;
 
 import org.slf4j.LoggerFactory;
 
@@ -28,7 +31,7 @@ public class MacIdProvider implements IdProvider {
 
     @Override
     public String provideId() {
-        String macAddress = null;
+        List<String> macAddresses = new ArrayList<String>();
         try {
             InetAddress localHost = InetAddress.getLocalHost();
             NetworkInterface ni = NetworkInterface.getByInetAddress(localHost);
@@ -36,20 +39,24 @@ public class MacIdProvider implements IdProvider {
                 // https://stackoverflow.com/questions/23900172/how-to-get-localhost-network-interface-in-java-or-scala
                 Enumeration<NetworkInterface> ne = NetworkInterface.getNetworkInterfaces();
                 while (ne.hasMoreElements()) {
-                    ni = ne.nextElement();
-                    break;
+                    ni = ne.nextElement();                    
+                    byte[] hardwareAddress = ni.getHardwareAddress();
+                    String[] hexadecimal = new String[hardwareAddress.length];
+                    for (int i = 0; i < hardwareAddress.length; i++) {
+                        hexadecimal[i] = String.format("%02X", hardwareAddress[i]);
+                    }
+                    macAddresses.add(String.join("", hexadecimal));
                 }
-            }
-            if (null != ni) {
-                byte[] hardwareAddress = ni.getHardwareAddress();
-                String[] hexadecimal = new String[hardwareAddress.length];
-                for (int i = 0; i < hardwareAddress.length; i++) {
-                    hexadecimal[i] = String.format("%02X", hardwareAddress[i]);
-                }
-                macAddress = String.join("", hexadecimal);
             }
         } catch (IOException e) {
             LoggerFactory.getLogger(MacIdProvider.class).error("Obtaining MAC-based device ID: " + e.getMessage());
+        }
+        String macAddress = null;
+        if (macAddresses.size() > 0) {
+            // network interfaces seems to change on Ubuntu/Docker after restart; sorting for a bit more dynamism
+            // selection criteria such as netmask could improve that
+            Collections.sort(macAddresses); 
+            macAddress = macAddresses.get(0);
         }
         return macAddress;
     }
