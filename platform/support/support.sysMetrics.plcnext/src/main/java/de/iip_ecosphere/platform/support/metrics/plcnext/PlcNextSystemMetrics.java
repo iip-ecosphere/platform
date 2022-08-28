f816bee9826f0698277355d76044bd0e852b225d
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2022-08-28 10:36:05 +0200

    initial version with GRPC access #51

--- platform/support/support.sysMetrics.plcnext/src/main/java/de/iip_ecosphere/platform/support/metrics/plcnext/PlcNextSystemMetrics.java
+++ platform/support/support.sysMetrics.plcnext/src/main/java/de/iip_ecosphere/platform/support/metrics/plcnext/PlcNextSystemMetrics.java
@@ -12,11 +12,15 @@
 
 package de.iip_ecosphere.platform.support.metrics.plcnext;
 
+import Arp.Device.Interface.Services.Grpc.IDeviceStatusServiceGrpc;
+import Arp.Device.Interface.Services.Grpc.IDeviceStatusServiceOuterClass.IDeviceStatusServiceGetItemRequest;
+import Arp.Device.Interface.Services.Grpc.IDeviceStatusServiceGrpc.IDeviceStatusServiceBlockingStub;
 import de.iip_ecosphere.platform.support.metrics.SystemMetrics;
 import io.grpc.ManagedChannel;
 import io.grpc.netty.NettyChannelBuilder;
-import io.grpc.netty.shaded.io.netty.channel.epoll.EpollDomainSocketChannel;
-import io.grpc.netty.shaded.io.netty.channel.unix.DomainSocketAddress;
+import io.netty.channel.epoll.EpollDomainSocketChannel;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.unix.DomainSocketAddress;
 
 /**
  * System metrics implementation for Phoenix Contact/PLCnext.
@@ -26,40 +30,69 @@ import io.grpc.netty.shaded.io.netty.channel.unix.DomainSocketAddress;
 public class PlcNextSystemMetrics implements SystemMetrics {
 
     public static final SystemMetrics INSTANCE = new PlcNextSystemMetrics();
+    private static final long TIMEOUT = 500;
+    
+    private static final IDeviceStatusServiceGetItemRequest REQUEST_BOARD_TEMPERATURE = 
+        IDeviceStatusServiceGetItemRequest
+            .newBuilder()
+            .setIdentifier("Status.Board.Temperature.Centigrade")
+            .build();
+    
+    private IDeviceStatusServiceBlockingStub client;
+    private long lastRequest = -1;
+    private float boardTemp = SystemMetrics.INVALID_CELSIUS_TEMPERATURE;
 
     /**
      * Prevents external creation.
      */
     protected PlcNextSystemMetrics() {
     }
-    
-    /*private void initialize() {
-        //https://www.plcnext.help/te/Service_Components/gRPC_Introduction.htm
-        ManagedChannel channel = NettyChannelBuilder.forAddress(new DomainSocketAddress("run/plcnext/grpc.sock"))
-            .eventLoopGroup(new EpollEventLoopGroup())
-            .channelType(EpollDomainSocketChannel.class)
-            .usePlaintext(true)
-            .build();
-        GrpcServicesGrpc.GrpcServicesBlockingStub client = GrpcServicesGrpc.newBlockingStub(channel);        
-    }*/
+
+    //https://www.plcnext.help/te/Service_Components/gRPC_Introduction.htm
+    // https://www.plcnext-community.net/makersblog/how-to-create-a-client-for-the-plcnext-control-grpc-server-in-c/
+
+    /**
+     * Initialize the channel.
+     */
+    private void request() {
+        if (lastRequest < 1 || System.currentTimeMillis() - lastRequest > TIMEOUT) {
+            if (null == client) {
+                ManagedChannel channel = NettyChannelBuilder
+                    .forAddress(new DomainSocketAddress("/run/plcnext/grpc.sock"))
+                    .eventLoopGroup(new EpollEventLoopGroup())
+                    .channelType(EpollDomainSocketChannel.class)
+                    .usePlaintext()
+                    //.usePlaintext(true)
+                    .build();
+                client = IDeviceStatusServiceGrpc.newBlockingStub(channel);
+                
+                
+            }
+            boardTemp = client.getItem(REQUEST_BOARD_TEMPERATURE).getReturnValue().getFloatValue();
+        }
+    }
     
     @Override
     public float getCaseTemperature() {
-        return 0; // TODO
+        request();
+        return boardTemp;
     }
 
     @Override
     public float getCpuTemperature() {
-        return 0; // TODO
+        request();
+        return INVALID_CELSIUS_TEMPERATURE; // TODO
     }
     
     @Override
     public int getNumGpuCores() {
+        request();
         return 0; // TODO
     }
     
     @Override
     public int getNumCpuCores() {
+        request();
         return 0;  // TODO
     }
 
