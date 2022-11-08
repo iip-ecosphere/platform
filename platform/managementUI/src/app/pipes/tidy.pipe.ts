fa42643d7d3afd9e9bec80502bcd36c4b331f061
ChrisKHil <cknappe@gmx.net>
2022-11-08 19:09:30 +0100

    on the resource detail view: values of -274 are now replaces with ?

--- platform/managementUI/src/app/pipes/tidy.pipe.ts
+++ platform/managementUI/src/app/pipes/tidy.pipe.ts
@@ -7,14 +7,25 @@ export class TidyPipe implements PipeTransform {
 
   transform(value: any): string {
     if (typeof(value) === 'string') {
-      let index = value.indexOf('value');
-      if(index != -1 && value.indexOf('{') != -1) {
-        value = value.substring(index + 7);
-        index = value.indexOf('}');
-        value = value.substring(0, index);
-        value = parseFloat(value);
+      // let index = value.indexOf('value');
+      // if(index != -1 && value.indexOf('{') != -1) {
+      //   value = value.substring(index + 7);
+      //   index = value.indexOf('}');
+      //   value = value.substring(0, index);
+      //   value = parseFloat(value);
+      // }
+
+      // let index2 = value.indexOf('-274');
+      // if(index2 != -1) {
+      //   console.log('hit!');
+      //   value = value.replace('-274', '?');
+      // }
+    } else if(typeof(value) ==='number'){
+      if(value == -274) {
+        value = '?';
       }
     }
+    console.log(value);
     return value;
   }
 
