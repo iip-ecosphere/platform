983691f3463feec6f72b461a84af970fc5b934c7
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2022-11-21 10:06:15 +0100

    enabling/testing excluded paths

--- platform/tools/tools.maven.dependencies/src/main/java/de/iip_ecosphere/platform/tools/maven/dependencies/CleaningUnpackMojo.java
+++ platform/tools/tools.maven.dependencies/src/main/java/de/iip_ecosphere/platform/tools/maven/dependencies/CleaningUnpackMojo.java
@@ -13,6 +13,8 @@
 package de.iip_ecosphere.platform.tools.maven.dependencies;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -37,23 +39,71 @@ public class CleaningUnpackMojo extends UnpackMojo {
      */
     @Parameter(required = false)
     private FileSet cleanup;
+
+    /**
+     * Determines excluded paths from the given paths, i.e., split the directories add them to {@code excluded}.
+     * 
+     * @param paths the paths to be excluded
+     * @param isFile whether path represents a file (than ignore the last path) or whether it is a folder
+     * @param excluded the excluded paths to be modified as a side effect
+     */
+    private void addExcludedPaths(String[] paths, boolean isFile, Set<String> excluded) {
+        for (String p : paths) {
+            addExcludedPaths(p, isFile, excluded);
+        }
+    }
+
+    /**
+     * Determines excluded paths from the given path, i.e., split the directories add them to {@code excluded}.
+     * 
+     * @param path the path to be excluded
+     * @param isFile whether path represents a file (than ignore the last path) or whether it is a folder
+     * @param excluded the excluded paths to be modified as a side effect
+     */
+    private void addExcludedPaths(String path, boolean isFile, Set<String> excluded) {
+        excluded.add(path);
+        String[] subPaths = path.replace("\\", "/").split("/");
+        if (subPaths.length > 0) {
+            String tmp = "";
+            for (int i = 0; i < subPaths.length - (isFile ? 1 : 0); i++) { // if isFile, not the file name
+                if (i > 0) {
+                    tmp += "/";
+                }
+                tmp += subPaths[i];
+                excluded.add(tmp);
+            }
+        }
+    }
+    
+    /**
+     * Deletes the given paths.
+     * 
+     * @param paths the paths to be deleted
+     * @param excluded paths that shall not be deleted (excluded)
+     */
+    private void deletePaths(String[] paths, Set<String> excluded) {
+        for (String p : paths) {
+            if (!excluded.contains(p)) {
+                File file = new File(cleanup.getDirectory(), p);
+                getLog().info("Deleting " + file);
+                FileUtils.deleteQuietly(file);
+            }
+        }
+    }
     
     @Override
     protected void doExecute() throws MojoExecutionException, MojoFailureException {
         if (null != cleanup) {
             FileSetManager fileSetManager = new FileSetManager();
-            String[] includedFiles = fileSetManager.getIncludedFiles(cleanup);
-            for (String f : includedFiles) {
-                File file = new File(cleanup.getDirectory(), f);
-                getLog().info("Deleting " + file);
-                FileUtils.deleteQuietly(file);
-            }
-            String[] includedDirs = fileSetManager.getIncludedDirectories(cleanup);
-            for (String f : includedDirs) {
-                File file = new File(cleanup.getDirectory(), f);
-                getLog().info("Deleting " + file);
-                FileUtils.deleteQuietly(file);
-            }
+            Set<String> excluded = new HashSet<>();
+            excluded.add("");  // don't delete containing directory, it's part of included directories
+            excluded.add(".");
+
+            addExcludedPaths(fileSetManager.getExcludedDirectories(cleanup), false, excluded);
+            addExcludedPaths(fileSetManager.getExcludedFiles(cleanup), true, excluded);
+
+            deletePaths(fileSetManager.getIncludedFiles(cleanup), excluded);
+            deletePaths(fileSetManager.getIncludedDirectories(cleanup), excluded);
         }
         super.doExecute();
     }
