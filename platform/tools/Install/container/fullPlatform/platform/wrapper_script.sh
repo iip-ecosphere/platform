2b6575b733d40db130a8b3142deceb34f18f45ed
Monika <you@example.com>
2022-11-25 14:12:36 +0100

    #91 first version

--- platform/tools/Install/container/fullPlatform/platform/wrapper_script.sh
+++ platform/tools/Install/container/fullPlatform/platform/wrapper_script.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
-  
+
 # turn on bash's job control
 set -m
 echo ">>> Starting broker..."
-./broker/broker.sh &
-echo ">>> Starting platform server..."
-./platform.sh &
+bash broker.sh &
+sleep 4;
+printf "\n\n>>> Starting platform server...\n"
+bash platform.sh &
 while [ ! -f /run/iip-platform.pid ]
 do
   sleep 1
 done
-echo ">>> Starting local ECS-Runtime..."
+printf "\n\n>>> Starting local ECS-Runtime...\n"
 ./ecs.sh &
 while [ ! -f /run/iip-ecsRuntime.pid ]
 do
   sleep 1
 done
-echo ">>> Starting local Service Manager..."
-./serviceMgr.sh
+printf "\n\n>>> Starting local Service Manager...\n"
+bash serviceMgr.sh &
 while [ ! -f /run/iip-serviceMgr.pid ]
 do
   sleep 1
 done
+printf "\n\n>>> Starting Monitoring...\n"
+bash monitoring.sh &
+while [ ! -f /run/iip-monitoring.pid ]
+do
+  sleep 1;
+done
+printf "\n\n>>> Starting UI...\n"
+bash mgtUi.sh
 
 fg %1
