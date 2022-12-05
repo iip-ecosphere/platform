111d1c3a2fc2620ee18b90a4cd921508877234ef
Weber <Weber@host.docker.internal>
2022-12-05 11:56:30 +0100

    Updated the template example to the new build steps.

--- platform/examples/examples.templates/examples.templates.impl/src/test/python/pyTest.py
+++ platform/examples/examples.templates/examples.templates.impl/src/test/python/pyTest.py
@@ -7,46 +7,43 @@ parser = argparse.ArgumentParser()
 parser.add_argument("relModelPath", help="relative path to the impl.model project")
 args = parser.parse_args()
 
-"""Further Notes: We should auto generate one template for each possible python service and make them callable separately 
-through maven -->> we shall generate one maven execution command like we did for the python test for each pyService!"""
-
-""" Paths are temporary set but seem to work like this paths can only be hardcoded from the /gen onwards! """
-""" Temp as reminder! :../../../../examples.templates.model """
-sys.path.insert(1, (args.relModelPath + '/gen/test/ApplicationInterfaces/src/main/python'))
-sys.path.insert(2, (args.relModelPath + '/gen/test/ApplicationInterfaces/src/main/python/iip'))
+""" will be the relativve paths from impl.impl to impl.model, given through pom /gen onwards! """
+sys.path.insert(1, (args.relModelPath))
+sys.path.insert(2, (args.relModelPath + "/iip"))
 """ always fixed value as these directorys are generated in the impl.impl """
-sys.path.insert(3, '../../main/python/services') 
+sys.path.insert(3, "../../main/python/services") 
 
 import json
 
 from PyService import PyService
+from datatypes.NewInput import NewInput
 from datatypes.NewInputImpl import NewInputImpl
-
+from datatypes.NewOutput import NewOutput
+from datatypes.NewOutputImpl import NewOutputImpl
 
 class TestPyServive(unittest.TestCase):
-    
-    def test_inputTest(self):
 
-        rawData = ''
-        with open ('../resources/testData-PyService.json', 'r') as f:
-            rawData = json.load(f)
-        impl = NewInputImpl()
-        impl.setType(rawData['newInput']['type'])
-        impl.setAirTemp(rawData['newInput']['airTemp'])
-        impl.setProcTemp(rawData['newInput']['procTemp'])
-        impl.setRotSpe(rawData['newInput']['rotSpe'])
-        impl.setTorq(rawData['newInput']['torq'])
-        impl.setToolWear(rawData['newInput']['toolWear'])
-        
-        """Quick fix for the wrong placement of the class (positioning would not allow for loading of resources otherwise)"""
-        os.chdir('../../main/python') 
-        
-        service = PyService()
-        service.processNewInput(impl)
-        """ processNewInput does not return anything not sure what to assert"""
+    def test_inputTest(self):
+    
+        #Change name to correct .json, access rawData for values i.e. rawData["InputType"]["Value1"]
+        rawData = ""
+        try :
+            with open ("../resources/testData-PyService.json", "r") as f:
+                rawData = json.load(f)
+        except:
+            print("You need to edit the template files in /src/test/resources to be correct json") 
+
+
+        # prepare your data, e.g., loading from json
+        """Needed to enable the loading of resources like the finished service would"""
+        os.chdir("../../main/python") 
+
+        # instantiate your service here, e.g., service = PyService()
+        # call your service here, e.g., service.processNewInput(impl)
+     
+        # do your asserts here
         self.assertTrue(True)
 
 if __name__ == "__main__":
-    """ actually dont know what this does..."""
-    unittest.main(argv=['first-arg-is-ignored'], exit=False)
-
+    """Makes unittest and argparse work together"""
+    unittest.main(argv=["first-arg-is-ignored"], exit=False)
