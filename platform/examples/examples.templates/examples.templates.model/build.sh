03f2572d0ca7f2682e9cd027a53db3a99d1a4794
Weber <Weber@weber7400.sse.local>
2022-12-15 16:40:10 +0100

    Added build.sh, updated readme

---
+++ platform/examples/examples.templates/examples.templates.model/build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#Explanation see README.MD
+rm -rf gen
+#build with broker
+dir=$PWD
+mvn -U generate-sources
+mvn install
+cd gen/py/templates/eclipse/impl.TestTestApp
+mvn install
+cd $dir
+
+mvn install
+
+
+#execute and test
+
+brokerPort=8883
+read LOWERPORT UPPERPORT < /proc/sys/net/ipv4/ip_local_port_range
+while :
+do
+        brokerPort="`shuf -i $LOWERPORT-$UPPERPORT -n 1`"
+        ss -lpn | grep -q ":$PORT " || break
+done
+echo "Using Broker Port: $brokerPort"
+
+
+cd gen/broker/broker
+./broker.sh $brokerPort &
+pidBroker=$!
+cd $dir
+
+echo "Broker PID $pidBroker"
+mvn -P App exec:java -Dexec.args="--iip.test.stop=30000 --iip.test.brokerPort=$brokerPort" > log &
+pidTest=$!
+echo "Test started $pidTest"
+
+sleep 30 && pkill -9 -P "$pidTest" && kill -9 "$pidTest"
+pkill -9 -P "$pidBroker" && kill -9 "$pidBroker"
+
+echo "Testing for RECEIVED in log"
+
+grep -Fq "RECEIVED" log
