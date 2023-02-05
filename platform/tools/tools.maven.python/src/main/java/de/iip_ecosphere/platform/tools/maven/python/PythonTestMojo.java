3943052a5849fc1c24e781294b5ae97682939de8
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2023-02-05 08:57:36 +0100

    disconnecting from platformDependencies -> release
    - avoid build cycle in release
    - take over PythonUtils

--- platform/tools/tools.maven.python/src/main/java/de/iip_ecosphere/platform/tools/maven/python/PythonTestMojo.java
+++ platform/tools/tools.maven.python/src/main/java/de/iip_ecosphere/platform/tools/maven/python/PythonTestMojo.java
@@ -27,8 +27,6 @@ import org.apache.maven.project.MavenProject;
 import org.apache.maven.shared.model.fileset.FileSet;
 import org.apache.maven.shared.model.fileset.util.FileSetManager;
 
-import de.iip_ecosphere.platform.support.PythonUtils;
-
 /**
  * Maven Mojo for Python unit tests.
  * 
