04a692319a7840e716fc0374985e0ff719b3f4d4
monika sse <jj@fff.com>
2023-02-06 09:37:13 +0100

    time between execution of the platform and other components extended

--- platform/tools/Install/container/DevContainer/ecs_starter.sh
+++ platform/tools/Install/container/DevContainer/ecs_starter.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 cd /root/platform
-while [ ! -f /run/iip-platform.pid ]
-do
-  sleep 1
-done
+sleep 15;
 printf "\n\n>>> Starting local ECS-Runtime...\n"
 bash ecs.sh --iip.id=fullPlatform >> logs/ecs.log
--- platform/tools/Install/container/DevContainer/serviceMgr_starter.sh
+++ platform/tools/Install/container/DevContainer/serviceMgr_starter.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 cd /root/platform
-while [ ! -f /run/iip-ecsRuntime.pid ]
-do
-  sleep 1
-done
+sleep 15;
 printf "\n\n>>> Starting local Service Manager...\n"
 bash serviceMgr.sh --iip.id=fullPlatform  >> logs/serviceMgr.log
