ee40a19f18074d3dda2fd5bf548b68c56d6c19c1
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2023-03-19 08:05:36 +0100

    TimeUtils.waitFor

--- platform/support/support/src/main/java/de/iip_ecosphere/platform/support/TimeUtils.java
+++ platform/support/support/src/main/java/de/iip_ecosphere/platform/support/TimeUtils.java
@@ -12,6 +12,8 @@
 
 package de.iip_ecosphere.platform.support;
 
+import java.util.function.Supplier;
+
 /**
  * Test utilities.
  * 
@@ -29,6 +31,30 @@ public class TimeUtils {
             Thread.sleep(ms);
         } catch (InterruptedException e) {
         }
+    }
+
+    /**
+     * A generic function that waits either until the {@code timeout} is reached or the {@code endCondition}
+     * returns {@code true}.
+     * 
+     * @param continueCondition function that returns {@code true} when waiting go on and {@code false} 
+     *     when waiting shall stop
+     * @param timeoutMs timeout from calling in ms, may be negative for endless waiting
+     * @param sleepMs time between two trials of {@code timeoutMs} and {@code endCondition}
+     * @return {@code true} if the {@code continueCondition} stopped waiting, {@code false} if the 
+     *     timeout caused the end of waiting
+     */
+    public static boolean waitFor(Supplier<Boolean> continueCondition, int timeoutMs, int sleepMs) {
+        boolean endConditionMet = false;
+        long start = System.currentTimeMillis();
+        while (timeoutMs < 0 || System.currentTimeMillis() - start < timeoutMs) {
+            endConditionMet = !continueCondition.get();
+            if (endConditionMet) {
+                break;
+            }
+            sleep(sleepMs);
+        }
+        return endConditionMet;
     }
 
 }
--- platform/support/support/src/test/java/test/de/iip_ecosphere/platform/support/TimeUtilsTest.java
+++ platform/support/support/src/test/java/test/de/iip_ecosphere/platform/support/TimeUtilsTest.java
@@ -13,6 +13,9 @@
 package test.de.iip_ecosphere.platform.support;
 
 import de.iip_ecosphere.platform.support.TimeUtils;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -24,7 +27,7 @@ import org.junit.Test;
 public class TimeUtilsTest {
 
     /**
-     * Tests the sleep method.
+     * Tests the {@link TimeUtils#sleep(int)} method.
      */
     @Test
     public void testSleep() {
@@ -33,6 +36,26 @@ public class TimeUtilsTest {
         long after = System.currentTimeMillis();
         long diff = after - before;
         Assert.assertTrue(80 < diff && diff < 200);
+    }
+    
+    /**
+     * Tests the {@link TimeUtils#waitFor(java.util.function.Supplier, int, int)} method.
+     */
+    @Test
+    public void testWaitFor() {
+        Assert.assertTrue(TimeUtils.waitFor(() -> false, 200, 100)); // return immediately
+        
+        AtomicInteger counter = new AtomicInteger(0);
+        // return after 5 rounds, but called to wait endlessly
+        Assert.assertTrue(TimeUtils.waitFor(() -> counter.incrementAndGet() < 5, -1, 100)); 
+
+        counter.set(0);
+        // return after 5 rounds, before timeout
+        Assert.assertTrue(TimeUtils.waitFor(() -> counter.incrementAndGet() < 5, 1000, 100)); 
+
+        counter.set(0);
+        // timeout happens before
+        Assert.assertFalse(TimeUtils.waitFor(() -> counter.incrementAndGet() < 10, 300, 100)); 
     }
 
 }
