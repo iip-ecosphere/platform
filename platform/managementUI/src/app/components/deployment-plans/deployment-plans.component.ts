dfdde154c9f33f0308abc0f3e28b43d78157b3e2
ChrisKHil <cknappe@gmx.net>
2023-04-16 15:49:04 +0200

    status box now properly gets task status and displays status

--- platform/managementUI/src/app/components/deployment-plans/deployment-plans.component.ts
+++ platform/managementUI/src/app/components/deployment-plans/deployment-plans.component.ts
@@ -89,8 +89,7 @@ export class DeploymentPlansComponent implements OnInit {
         if(value) {
           params[0].value.value = value.value;
         }
-        //const response = await this.deployer.deployPlan(params, true);
-        const response = await this.deployer.deployPlan(params);
+        const response = await this.deployer.deployPlan(params, true);
       }
     }
 
