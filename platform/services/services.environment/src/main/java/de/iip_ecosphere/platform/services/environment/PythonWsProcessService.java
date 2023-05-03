6c74e32cd868441c839db993c9cc6789483bb16a
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2023-05-03 19:07:36 +0200

    different connect strategy, ok for Linux?

--- platform/services/services.environment/src/main/java/de/iip_ecosphere/platform/services/environment/PythonWsProcessService.java
+++ platform/services/services.environment/src/main/java/de/iip_ecosphere/platform/services/environment/PythonWsProcessService.java
@@ -21,6 +21,7 @@ import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.enums.ReadyState;
 import org.java_websocket.exceptions.WebsocketNotConnectedException;
 import org.java_websocket.handshake.ServerHandshake;
 import org.slf4j.Logger;
@@ -52,7 +53,6 @@ public class PythonWsProcessService extends PythonAsyncProcessService {
     private long averageResponseTime = 0;
     private WebSocket socket;
     private String networkPortKey;
-    private String lastError; // new instance of WebSocket per try
 
     /**
      * Creates an instance from a service id and a YAML artifact.
@@ -109,20 +109,17 @@ public class PythonWsProcessService extends PythonAsyncProcessService {
     
     @Override
     protected void createScanInputThread(Process proc) {
-        while (null == socket) { 
-            try {
-                WebSocket tmp = new WebSocket(new URI("ws://localhost:" + instancePort));
-                if (tmp.connectBlocking()) { // blocking may fail
-                    getLogger().info("Connected to port {}", instancePort);
-                    socket = tmp;
-                }
-            } catch (URISyntaxException | InterruptedException e) {
-                getLogger().error("Connecting to port {}: {}", instancePort, e.getMessage());
-            } 
-            if (null == socket) {
-                TimeUtils.sleep(250);
-            }
-        }
+        String uri = "ws://localhost:" + instancePort;
+        try {
+            getLogger().info("Connecting to {}", uri);
+            WebSocket tmp = new WebSocket(new URI(uri));
+            tmp.connect();
+            TimeUtils.waitFor(() -> tmp.getReadyState() != ReadyState.OPEN, -1, 250);
+            getLogger().info("Connected to {}", uri);
+            socket = tmp;
+        } catch (URISyntaxException e) {
+            getLogger().error("Connecting to {}: {}", uri, e.getMessage());
+        } 
     }
     
     @Override
@@ -170,6 +167,8 @@ public class PythonWsProcessService extends PythonAsyncProcessService {
      * @author Holger Eichelberger, SSE
      */
     private class WebSocket extends WebSocketClient {
+
+        private String lastError;
 
         /**
          * Creates a web socket for the given server URI.
