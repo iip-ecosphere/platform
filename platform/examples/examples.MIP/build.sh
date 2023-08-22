34b1d097001f6a7300d1d86022cdbd3a5b42db6b
alamoush <alamoush@reserve7400.sse.local>
2023-08-22 15:12:13 +0200

    Add examples.MIP to Github

---
+++ platform/examples/examples.MIP/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#Explanation see README.MD
+
+rm -rf gen
+
+#build with broker
+
+mvn -U install -Dunpack.force=true
+
+#execute and test
+
+#brokerPort=8883
+#read LOWERPORT UPPERPORT < /proc/sys/net/ipv4/ip_local_port_range
+#while :
+#do
+#        brokerPort="`shuf -i $LOWERPORT-$UPPERPORT -n 1`"
+#        ss -lpn | grep -q ":$PORT " || break
+#done
+#echo "Using Broker Port: $brokerPort"
+
+#dir=$PWD
+#cd gen/broker/broker
+#./broker.sh $brokerPort &
+#pidBroker=$!
+#cd $dir
+
+#echo "Broker PID $pidBroker"
+#mvn -P App exec:java -Diip.springStart.args="--iip.test.stop=30000 --iip.test.brokerPort=$brokerPort" > log &
+#pidTest=$!
+#echo "Test started $pidTest"
+
+#sleep 30 && pkill 9 -P "$pidTest" && kill -9 "$pidTest"
+#pkill -9 -P "$pidBroker" && kill -9 "$pidBroker"
+
+#echo "Testing for RECEIVED in log"
+
+grep -Fq "RECEIVED" log
+
