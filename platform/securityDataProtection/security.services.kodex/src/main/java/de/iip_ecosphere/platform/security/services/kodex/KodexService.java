e71c2c88a2a4e525f3a5ab37bc5723e99d03baa2
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2023-09-08 11:40:07 +0200

    win shorter waiting time, new multi-type service

--- platform/securityDataProtection/security.services.kodex/src/main/java/de/iip_ecosphere/platform/security/services/kodex/KodexService.java
+++ platform/securityDataProtection/security.services.kodex/src/main/java/de/iip_ecosphere/platform/security/services/kodex/KodexService.java
@@ -38,7 +38,7 @@ import de.iip_ecosphere.platform.transport.serialization.TypeTranslator;
  */
 public class KodexService<I, O> extends AbstractStringProcessService<I, O>  {
 
-    public static final int WAITING_TIME_WIN = 120000; // preliminary
+    public static final int WAITING_TIME_WIN = 100; // preliminary
     public static final int WAITING_TIME_OTHER = 100; // preliminary
     public static final String VERSION = "0.1.6";
     private static final boolean DEBUG = false;
