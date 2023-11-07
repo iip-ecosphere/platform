9f9674b495b76ec0a8d88cd0579910566f517285
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2023-11-07 07:59:02 +0100

    windows script call detection

--- platform/configuration/configuration.maven/src/main/java/de/iip_ecosphere/platform/configuration/maven/ProcessUnit.java
+++ platform/configuration/configuration.maven/src/main/java/de/iip_ecosphere/platform/configuration/maven/ProcessUnit.java
@@ -413,7 +413,11 @@ public class ProcessUnit {
          * @return <b>this</b> (builder style)
          */
         public ProcessUnitBuilder addArgumentOrScriptCommand(boolean cmdAsScript, String cmd) {
-            if (!SystemUtils.IS_OS_WINDOWS && SCRIPT_NAMES.contains(cmd)) {
+            if (SCRIPT_NAMES.contains(cmd)) {
+                if (SystemUtils.IS_OS_WINDOWS) {
+                    addArguments(ProcessUnit.WIN_BAT_PREFIX);
+                    enableArgumentAggregation();
+                }
                 addArgument(cmd);
             } else if (cmdAsScript) {
                 addShellScriptCommand(cmd);
