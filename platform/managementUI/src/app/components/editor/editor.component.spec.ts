d5ab5e1c6c5c788b0fd23ada197e2b2a6ff7eee3
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2023-11-25 09:15:56 +0100

    refined testing, more testing

--- platform/managementUI/src/app/components/editor/editor.component.spec.ts
+++ platform/managementUI/src/app/components/editor/editor.component.spec.ts
@@ -13,11 +13,8 @@ describe('EditorComponent', () => {
 
   let component: EditorComponent;
   let fixture: ComponentFixture<EditorComponent>;
-  var originalTimeout : number;
 
   beforeEach(async () => {
-    originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
     await EnvConfigService.initAsync();
     await TestBed.configureTestingModule({
       imports: [ HttpClientModule, FormsModule, MatTooltipModule ],
@@ -38,95 +35,243 @@ describe('EditorComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should handle Server', async() => {
-    await test(fixture, component, "Servers", ["JavaServer", "PythonServer", "FlowerFederatedAiServer"], 0, [
-      {name:"class", kind:"text"}, {name:"cmdArg", kind:"text"}, {name:"memLimit", kind:"text"}, 
-      {name:"id", kind:"text"}, {name:"description", kind:"text"}, {name:"host", kind:"text"}, 
-      {name:"port", kind:"text"}, {name:"executable", kind:"text"}, {name:"nameplateInfo", kind:"sub"}, 
-      {name:"artifact", kind:"text"}, {name:"running", kind:"bool"}, {name:"parameter", kind:"text"}, 
-      {name:"transportChannel", kind:"text"}, {name:"ver", kind:"text"}
-    ]);
+  it('should handle Constants', async() => {
+    let expectedTypes = [
+      {name: "String", input:[
+        {name: "value", kind: InputKind.text}
+      ]}, 
+      {name: "Boolean", input:[
+        {name: "value", kind: InputKind.bool}
+      ]}, 
+      {name: "Real", input:[
+        {name: "value", kind: InputKind.text}
+      ]}, 
+      {name: "Integer", input:[
+        {name: "value", kind: InputKind.text}
+      ]}
+    ];
+    await test(fixture, component, "Constants", expectedTypes, false);
   });
 
-  afterEach(() => {
-    if (originalTimeout) {
-      jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
-    }
+  it('should handle Types', async() => {
+    let expectedTypes = [
+      {name: "UAMethodType"}, 
+      {name: "UARootMethodType"}, 
+      {name: "UARootVariableType"}, 
+      {name: "UAObjectTypeType"}, 
+      {name: "UAVariableTypeType"}, 
+      {name: "UADataType"}, 
+      {name: "ArrayType", input:[
+        {name: "name", kind: InputKind.text}, {name: "type", kind: InputKind.ref}
+      ]}, 
+      {name: "RecordType", input:[
+        {name: "name", kind: InputKind.text}, {name: "fields", kind: InputKind.text} // TODO fix
+      ]}, 
+      {name: "ByteArrayTypeType"}, 
+      {name: "IntegerArrayTypeType"}, 
+      {name: "DoubleArrayTypeType"}, 
+      {name: "StringArrayTypeType"}];
+    await test(fixture, component, "Types", expectedTypes, false);
+  });
+
+  it('should handle Dependencies', async() => {
+    let expectedTypes = [
+      {name: "SystemDependency"}, 
+      {name: "LinuxSystemDependency", input:[
+        {name: "name", kind: InputKind.text}, {name: "key", kind: InputKind.text}
+      ]}, 
+      {name: "LinuxCommandBasedSystemDependency"}, 
+      {name: "PythonDependency", input:[
+        {name: "name", kind: InputKind.text}, {name: "version", kind: InputKind.text}
+      ]}, 
+      {name: "MavenDependency"}
+    ];
+    await test(fixture, component, "Dependencies", expectedTypes, false);
+  });
+
+  it('should handle Nameplates', async() => {
+    let expectedTypes = [
+      {name: "NameplateInfo", input: [
+        {name: "productInfo", kind: InputKind.text}, {name: "manufacturerProductDesignation", kind: InputKind.text},
+        {name: "productImage", kind: InputKind.text}, {name: "manufacturer", kind: InputKind.ref}
+      ]}
+    ];
+    await test(fixture, component, "Nameplates", expectedTypes, false);
+  });
+
+  it('should handle Services', async() => {
+    let serviceInput = [{name:"class", kind:InputKind.text}, {name:"artifact", kind:InputKind.text},
+      {name:"description", kind:InputKind.text}, {name:"deployable", kind:InputKind.bool},
+      {name:"ver", kind:InputKind.text}, {name:"id", kind:InputKind.text}, 
+      {name:"name", kind:InputKind.text}, {name:"kind", kind:InputKind.enum},
+      {name:"asynchronous", kind:InputKind.bool}, {name:"nameplateInfo", kind:InputKind.sub}]; 
+    let expectedTypes = [
+      {name: "TraceToAasService"}, 
+      {name: "TraceToAasJavaService"}, 
+      {name: "Connector"}, // TODO abstract?
+      {name: "ChannelConnector"}, 
+      {name: "OpcUaV1Connector"}, 
+      {name: "AasConnector"}, 
+      {name: "MqttV3Connector", input: serviceInput.concat([
+        {name:"port", kind:InputKind.text}, {name:"deviceServiceKey", kind:InputKind.text},
+        {name:"inSerializerClass", kind:InputKind.text}, {name:"outSerializerClass", kind:InputKind.text},
+        {name:"inChannel", kind:InputKind.text}, {name:"outChannel", kind:InputKind.text},
+        {name:"machineFormatter", kind:InputKind.sub}, {name:"machineParser", kind:InputKind.sub},
+        {name:"encoding", kind:InputKind.enum}, {name:"host", kind:InputKind.text},
+        {name:"inAdapterClass", kind:InputKind.text}, {name:"outAdapterClass", kind:InputKind.text},
+        {name:"inInterface", kind:InputKind.sub}, {name:"outInterface", kind:InputKind.sub},
+        {name:"samplingPeriod", kind:InputKind.text}, {name:"security", kind:InputKind.sub},
+        {name:"mock", kind:InputKind.bool}, {name:"cacheMode", kind:InputKind.enum},
+        {name:"parameter", kind:InputKind.text} // TODO check
+      ])}, 
+      {name: "MqttV5Connector"}, 
+      {name: "MqttConnector"}, 
+      {name: "JavaService", input: serviceInput}, 
+      {name: "PythonService"}, 
+      {name: "FlowerFederatedAiService"}, 
+      {name: "KodexService"}, 
+      {name: "RtsaService"}, 
+      {name: "MipMqttV3Connector"}, 
+      {name: "NovoAIMqttV3Connector"}];
+      await test(fixture, component, "Services", expectedTypes, false);
+  });
+
+  it('should handle Server', async() => {
+    let expectedTypes = [{
+      name:"JavaServer", input: [
+        {name:"class", kind:InputKind.text}, {name:"cmdArg", kind:InputKind.text}, 
+        {name:"memLimit", kind:InputKind.text}, {name:"id", kind:InputKind.text}, 
+        {name:"description", kind:InputKind.text}, {name:"host", kind:InputKind.text}, 
+        {name:"port", kind:InputKind.text}, {name:"executable", kind:InputKind.text}, 
+        {name:"nameplateInfo", kind:InputKind.sub}, {name:"artifact", kind:InputKind.text}, 
+        {name:"running", kind:InputKind.bool}, {name:"parameter", kind:InputKind.text}, 
+        {name:"transportChannel", kind:InputKind.text}, {name:"ver", kind:InputKind.text}]}, 
+      {name:"PythonServer"}, 
+      {name:"FlowerFederatedAiServer"}];
+    await test(fixture, component, "Servers", expectedTypes, false);
+  });
+
+  it('should handle Meshes', async() => {
+    let expectedTypes = [
+      {name: "ServiceMesh", input:[
+        {name:"description", kind:InputKind.text}
+      ]}
+    ];
+    await test(fixture, component, "Meshes", expectedTypes, false);
+  });
+
+  it('should handle Applications', async() => {
+    let expectedTypes = [
+      {name: "Application", input:[
+        {name:"id", kind: InputKind.text}, {name:"name", kind:InputKind.text}, 
+        {name:"description", kind:InputKind.text}, {name:"artifact", kind:InputKind.text}, 
+        {name:"nameplateInfo", kind:InputKind.sub}, {name:"debug", kind: InputKind.bool},
+        {name:"cmdArg", kind:InputKind.text}, {name:"ver", kind:InputKind.text}, 
+        {name:"snapshot", kind:InputKind.bool}, {name:"createContainer", kind:InputKind.bool}
+      ]}
+    ];
+    await test(fixture, component, "Applications", expectedTypes, false);
   });
 
 });
 
+interface TypeProps {
+  name:string
+  input?: InputProps[] 
+}
+
 interface InputProps {
   name: string
-  kind: string
+  kind: InputKind
+}
+
+enum InputKind {
+  text, // text input
+  bool, // boolean selector
+  enum, // boolean selector
+  sub,  // type sub-editor 
+  ref   // reference selector
 }
 
 async function test(fixture: ComponentFixture<EditorComponent>, component: EditorComponent, category: string, 
-  types: string[], selectedType: number, props: InputProps[]) {
+  typeProps: TypeProps[], debug:boolean) {
+
   component.category = category;
   await component.ngOnInit();
   await fixture.detectChanges();
 
+  expect(component).toBeTruthy();
   let compiled = fixture.nativeElement as HTMLElement;
 
-  let expectedItems = new Set<string>(types);
-  let typeSelect = compiled.querySelector('mat-select[id="typeSelect"]') as HTMLElement;
-  expect(typeSelect).toBeTruthy();
-  let typeOptions = typeSelect.querySelectorAll('mat-option') as NodeListOf<Element>;
-  expect(typeOptions).toBeTruthy();
-  typeOptions.forEach((o) => {
-    expectedItems.delete(o.innerHTML.trim());
-  });
-  expect(expectedItems.size).withContext("Expected types shall all be present").toBe(0);
-  component.selectedType = component.meta?.value?.find(type => type.idShort === types[selectedType]);
-  component.generateInputs();
-
-  await fixture.detectChanges();
-  await fixture.whenRenderingDone();
-
-//console.log(fixture.nativeElement);
-
-  let inputs = compiled.querySelector('div[class="inputGroup"]') as HTMLElement;
-//console.log("INPUTS " + inputs);
-//console.log("UIGROUPS " + component.uiGroups);
-//console.log("CATEGORY " + component.category);
-//console.log("TYPE " + component.type);
-//console.log("DROP " + component.showDropdown);
-//console.log("META " + component.meta);
-//console.log("META-V " + component.meta?.value);
-  expect(inputs).toBeTruthy();
-  var uiGroups = inputs.children;
-  for (var g = 0; g < uiGroups.length; g++) {
-    var uiGroup = uiGroups[g] as HTMLElement;
-    var inputName = uiGroup.querySelector('p[id="inputName"]') as HTMLElement;
-    expect(inputName).toBeTruthy();
-    var varName = inputName.innerText.trim();
-
-    var expectedEditor = props.find(p => p.name === varName);
-    var textEditor = uiGroup.querySelector('mat-label') as HTMLElement;
-    var subEditor = uiGroup.querySelector('app-subeditor-button') as HTMLElement;
-    var boolEditor = uiGroup.querySelector('app-boolean-dropdown') as HTMLElement;
-    var enumEditor = uiGroup.querySelector('app-enum-dropdown') as HTMLElement;
-    var refEditor = uiGroup.querySelector('app-input-ref-select') as HTMLElement;
-  if (expectedEditor) {
-      if (textEditor) {
-        var input = uiGroup.querySelector('input') as HTMLElement;
-        expect(input).withContext(varName).toBeTruthy();
-        expect(expectedEditor?.kind).withContext(varName).toBe("text");
-      } else if (boolEditor) {
-        expect(expectedEditor?.kind).withContext(varName).toBe("bool");
-      } else if (enumEditor) {
-        expect(expectedEditor?.kind).withContext(varName).toBe("enum");
-      } else if (subEditor) {
-        expect(expectedEditor?.kind).withContext(varName).toBe("sub");
-      } else if (refEditor) {
-        expect(expectedEditor?.kind).withContext(varName).toBe("ref");
+  if (typeProps.length > 1) { // only selection if multiple
+    let expectedItems = new Set<string>(typeProps.map(p => p.name));
+    let typeSelect = compiled.querySelector('mat-select[id="typeSelect"]') as HTMLElement;
+    expect(typeSelect).toBeTruthy();
+    let typeOptions = typeSelect.querySelectorAll('mat-option') as NodeListOf<Element>;
+    expect(typeOptions).toBeTruthy();
+    typeOptions.forEach((o) => {
+      if (debug) {
+        console.log("T-TYPE " + o.innerHTML.trim());    
       }
-    } else {
-console.log("UNKNOWN " + inputName.innerText.trim() + " text " + (!textEditor)+ " bool " + (!boolEditor)+ " sub " 
-  + (!subEditor)+ " enum " + (!enumEditor)+ " ref " + (!refEditor));
-    } 
+      expectedItems.delete(o.innerHTML.trim());
+    });
+    expect(expectedItems.size).withContext("Expected types shall all be present: " 
+      + Array.from(expectedItems).join(', ')).toBe(0);
+  } else if (typeProps.length == 1) {
+    expect(component.meta?.value?.length).toBe(1);
+    let ra = component.meta?.value;
+    if (ra) {
+      if (debug) {
+        console.log("T-TYPE " + typeProps[0].name);    
+      }
+      expect(ra[0].idShort).toEqual(typeProps[0].name);
+    }
   }
+  for (let typeProp of typeProps.filter(p => p.input)) {
+    component.selectedType = component.meta?.value?.find(type => type.idShort === typeProp.name);
+    component.generateInputs();
 
-  expect(component).toBeTruthy();
+    await fixture.detectChanges();
+    await fixture.whenRenderingDone();
+  
+//console.log(fixture.nativeElement);
+  
+    let inputs = compiled.querySelector('div[class="inputGroup"]') as HTMLElement;
+    expect(inputs).toBeTruthy();
+    var uiGroups = inputs.children;
+    for (var g = 0; g < uiGroups.length; g++) {
+      var uiGroup = uiGroups[g] as HTMLElement;
+      var inputName = uiGroup.querySelector('p[id="inputName"]') as HTMLElement;
+      expect(inputName).toBeTruthy();
+      var varName = inputName.innerText.trim();
+
+      var expectedEditor = typeProp.input?.find(p => p.name === varName);
+      var textEditor = uiGroup.querySelector('mat-label') as HTMLElement;
+      var subEditor = uiGroup.querySelector('app-subeditor-button') as HTMLElement;
+      var boolEditor = uiGroup.querySelector('app-boolean-dropdown') as HTMLElement;
+      var enumEditor = uiGroup.querySelector('app-enum-dropdown') as HTMLElement;
+      var refEditor = uiGroup.querySelector('app-input-ref-select') as HTMLElement;
+      if (expectedEditor) {
+        if (textEditor) {
+          var input = uiGroup.querySelector('input') as HTMLElement;
+          expect(input).withContext(varName).toBeTruthy();
+          expect(expectedEditor?.kind).withContext(varName + " shall be text").toBe(InputKind.text);
+        } else if (boolEditor) {
+          expect(expectedEditor?.kind).withContext(varName + " shall be bool").toBe(InputKind.bool);
+        } else if (enumEditor) {
+          expect(expectedEditor?.kind).withContext(varName + " shall be enum").toBe(InputKind.enum);
+        } else if (subEditor) {
+          expect(expectedEditor?.kind).withContext(varName + " shall be sub").toBe(InputKind.sub);
+        } else if (refEditor) {
+          expect(expectedEditor?.kind).withContext(varName + " shall be ref").toBe(InputKind.ref);
+        }
+      } else {
+        if (debug) {
+          console.log("UNKNOWN " + inputName.innerText.trim() + " text " + (!textEditor)+ " bool " + (!boolEditor)+ " sub " 
+            + (!subEditor)+ " enum " + (!enumEditor)+ " ref " + (!refEditor));
+        }
+      }
+      // TODO buttons 
+    }
+  }
 }
