e853df8214b5f22474efa5775d51f75799bba6a1
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2023-12-18 19:19:06 +0100

    documentation

--- platform/tools/tools.maven.python/src/main/java/de/iip_ecosphere/platform/tools/maven/python/FilesetUtils.java
+++ platform/tools/tools.maven.python/src/main/java/de/iip_ecosphere/platform/tools/maven/python/FilesetUtils.java
@@ -1,3 +1,15 @@
+/**
+ * ******************************************************************************
+ * Copyright (c) {2023} The original author or authors
+ *
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Public License 2.0 which is available 
+ * at http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: Apache-2.0 OR EPL-2.0
+ ********************************************************************************/
+
 package de.iip_ecosphere.platform.tools.maven.python;
 
 import java.io.File;
@@ -14,6 +26,11 @@ import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.shared.model.fileset.FileSet;
 import org.apache.maven.shared.model.fileset.util.FileSetManager;
 
+/**
+ * Helper functions for maven filesets.
+ * 
+ * @author Holger Eichelberger, SSE
+ */
 public class FilesetUtils {
 
     /**
