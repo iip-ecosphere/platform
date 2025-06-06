464d6c5c99b92b94820af6cfee29d70944f99877
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2025-06-06 13:51:47 +0200

    outdated unpacking

--- platform/examples/examples.MIP/src/test/python/iip/Registry.py
+++
@@ -1,8 +0,0 @@
-services = {} # service id to service
-serializers = {} # symbolic type name to SerializerToString
-types = {}   # python type to symbolic type name  
-receivers = {}
-senders = {}
-asyncTransformers = {}
-syncTransformers = {}
-netMgtKeyAddresses = {}
