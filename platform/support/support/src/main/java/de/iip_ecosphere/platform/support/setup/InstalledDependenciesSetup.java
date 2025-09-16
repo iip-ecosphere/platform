6379bade4d68f6857f1c78595f639faed14d8f93
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2025-09-16 11:47:25 +0200

    keep original for now

--- platform/support/support/src/main/java/de/iip_ecosphere/platform/support/setup/InstalledDependenciesSetup.java
+++ platform/support/support/src/main/java/de/iip_ecosphere/platform/support/setup/InstalledDependenciesSetup.java
@@ -19,6 +19,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
+import org.apache.commons.lang3.SystemUtils;
+
 import de.iip_ecosphere.platform.support.resources.FolderResourceResolver;
 import de.iip_ecosphere.platform.support.OsUtils;
 import de.iip_ecosphere.platform.support.logging.LoggerFactory;
@@ -86,7 +88,7 @@ public class InstalledDependenciesSetup extends AbstractSetup {
         if (OsUtils.isWindows()) {
             exeSuffix = ".exe";
         }
-        File javaPath = new File(OsUtils.getJavaHome(), "bin/java" + exeSuffix);
+        File javaPath = new File(SystemUtils.getJavaHome(), "bin/java" + exeSuffix);
         addDefaultEntry(getJavaKey(), javaPath);
         addDefaultEntry(KEY_PREFIX_JAVA, javaPath);
         addDefaultEntry(KEY_PREFIX_PYTHON, new File("python")); // generic python in path
@@ -111,7 +113,7 @@ public class InstalledDependenciesSetup extends AbstractSetup {
      * @return the key
      */
     public static String getJavaKey() {
-        String ver = OsUtils.getJavaSpecificationVersion();
+        String ver = SystemUtils.JAVA_SPECIFICATION_VERSION;
         if (ver.startsWith("1.")) {
             ver = ver.substring(2);
         }
