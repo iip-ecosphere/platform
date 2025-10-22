22ad8c4c3f8716083997e5ecf1069dec6f075941
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2025-10-22 11:37:22 +0200

    cleanup

--- platform/services/services.spring/src/test/java/test/de/iip_ecosphere/platform/services/spring/package-info.java
+++ platform/services/services.spring/src/test/java/test/de/iip_ecosphere/platform/services/spring/package-info.java
@@ -1,4 +1,9 @@
 /**
- * Tests for the ECS runtime.
+ * Tests for the spring-based implementation of the service manager. This folder contains some basic tests that can be 
+ * executed independently of the plugin character of this project. However, real execution tests can only be conducted
+ * in an environment with a clean root class loader (only platform layers, dependencies only via plugins). This is not
+ * possible here and happens in the subproject services.spring.plugintests (based on the simple test application in 
+ * test.simpleStream.spring). The actual tests are implemented here, provided through the SpringTestProviderDescriptor
+ * to services.spring.plugintests and executed there.
  */
 package test.de.iip_ecosphere.platform.services.spring;
