4f7ee76b448ecc7855055c7b397dd7222a540590
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2026-01-29 11:13:12 +0100

    peparing reusable cleanup

---
+++ platform/cleanup.sh
@@ -0,0 +1,39 @@
+#/bin/bash
+
+IMGS=$(docker images -qa -f 'dangling=true')
+echo "Cleaning up dangling docker images: $IMGS"
+if [ ! -z "$IMGS" ]
+then
+    docker rmi -f "$IMGS"
+fi
+
+IMGS=$(docker images -qa "simplemeshtestingcontainerapp/*")
+echo "Cleaning up IIP-Ecosphere configuration.configuration test images: $IMGS"
+if [ ! -z "$IMGS" ]
+then
+    docker rmi -f "$IMGS" 
+fi
+
+IMGS=$(docker images -qa "allapps/*")
+echo "Cleaning up IIP-Ecosphere allapps test images: $IMGS"
+if [ ! -z "$IMGS" ]
+then
+    docker rmi -f "$IMGS" 
+fi
+
+IMGS=$(docker images -qa "platform/*")
+echo "Cleaning up IIP-Ecosphere platform test images: $IMGS"
+if [ ! -z "$IMGS" ]
+then
+    docker rmi -f "$IMGS" 
+fi
+
+docker system prune -f --volumes
+rm -rf /tmp/tomcat-docbase.*
+rm -rf /tmp/tomcat.*
+rm -rf /tmp/qmProfiling*
+rm -rf /tmp/basyx-temp*
+rm -f /tmp/services.spring*.xml
+rm -f /tmp/platform-*.aasx
+rm -f /tmp/librocksdbjni*.so
+rm -f /tmp/examples-test-*.out
