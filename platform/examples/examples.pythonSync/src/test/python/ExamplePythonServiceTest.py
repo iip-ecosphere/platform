e6adf5a9778eda8bea02b3b4395b8ab113ef3519
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2026-03-06 16:35:55 +0100

    strange python inconsistency

--- platform/examples/examples.pythonSync/src/test/python/ExamplePythonServiceTest.py
+++ platform/examples/examples.pythonSync/src/test/python/ExamplePythonServiceTest.py
@@ -5,12 +5,12 @@ import argparse
 from TestUtils import runTestsFromFile, runTestsFromTestFile, readTestDataJson, runAllTestsFromFile, getListOfDeserializedData
 from Service import ServiceState
 import json
-from ExamplePythonService import ExamplePythonService
-from datatypes.PythonTestInput import PythonTestInput
-from datatypes.PythonTestInputImpl import PythonTestInputImpl
-from datatypes.PythonTestOutput import PythonTestOutput
-from datatypes.PythonTestOutputImpl import PythonTestOutputImpl
-from serializers.PythonTestInputSerializer import PythonTestInputSerializer
+from ExamplePythonService import ExamplePythonSyncService
+from datatypes.PythonSyncTestInput import PythonSyncTestInput
+from datatypes.PythonSyncTestInputImpl import PythonSyncTestInputImpl
+from datatypes.PythonSyncTestOutput import PythonSyncTestOutput
+from datatypes.PythonSyncTestOutputImpl import PythonSyncTestOutputImpl
+from serializers.PythonSyncTestInputSerializer import PythonSyncTestInputSerializer
 
 os.chdir(os.environ['PRJ_HOME'] + "/src/main/python")
 
@@ -18,10 +18,10 @@ class ExamplePythonServiceTest(unittest.TestCase):
     def test_inputTest(self):
         # Change name to correct .json, access rawData for values i.e. rawData["InputType"]["Value1"]
         rawData = ""
-        service = ExamplePythonService()
+        service = ExamplePythonSyncService()
         service.setState(ServiceState.STARTING)
         service.setState(ServiceState.RUNNING)
-        serializerPythonTestInput = PythonTestInputSerializer()
+        serializerPythonTestInput = PythonSyncTestInputSerializer()
         service.attachIngestor(assertionIngestor)
         runAllTestsFromFile(service.getId(), os.environ['PRJ_HOME'] + "/src/test/resources/testData-ExamplePythonService.json")
         
