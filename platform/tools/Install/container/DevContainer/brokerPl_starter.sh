455ef9b308e0f8bda66123fc02c0b1df595f8566
alamoush@sse.uni-hildesheim.de <alamoush@sse.uni-hildesheim.de>
2026-04-16 14:35:24 +0200

    VSCode Python extension

--- platform/tools/Install/container/DevContainer/brokerPl_starter.sh
+++ platform/tools/Install/container/DevContainer/brokerPl_starter.sh
@@ -6,15 +6,15 @@ LOG_DIR="/root/platform/logs"
 
 mkdir -p "$LOG_DIR"
 
-
-# Install VSCode Python extension if missing
-yes | code --install-extension ms-python.python --no-sandbox --user-data-dir=/root/.vscode
-
-sleep 7
 echo ">>> Starting broker..."
 bash "$SCRIPT_DIR/broker.sh" >> "$LOG_DIR/broker.log" 2>&1 &
 
 sleep 4
 echo ">>> Starting platform server..."
-bash "$SCRIPT_DIR/platform.sh" >> "$LOG_DIR/pl.log" 2>&1
+bash "$SCRIPT_DIR/platform.sh" >> "$LOG_DIR/pl.log" 2>&1 &
+PL_PID=$!
 
+# Install VSCode Python extension if missing
+yes | code --install-extension ms-python.python --no-sandbox --user-data-dir=/root/.vscode
+
+wait $PL_PID
